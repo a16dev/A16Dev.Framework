8f31a956b71b527fe026731e917946d8351c9163
Alexandre Peixoto <72051321+a16dev@users.noreply.github.com>
2023-09-02 20:31:15 -0300

    Add files via upload

---
+++ A16Dev.Data.Abstractions/IDataEntity.cs
@@ -0,0 +1,13 @@
+﻿using A16Dev.Domain.Abstractions;
+
+namespace A16Dev.Data.Abstractions
+{
+    /// <summary>
+    /// Generic marker interface for a data model.
+    /// Used to specifically identify data (persistence-related) models
+    /// </summary>
+    /// <typeparam name="TId">The type of Id</typeparam>
+    public interface IDataEntity<TId> : IEntity<TId>
+    {
+    }
+}
---
+++ A16Dev.Data.Abstractions/Repositories/IRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace A16Dev.Data.Abstractions
+{
+    public interface IRepository
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
